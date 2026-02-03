ed0bbc0e02a27766322cc0a44a5d9529b00796ba
MacBook <macbook@MacBook-Pro.local>
2026-02-02 16:29:28 -0800

    Add Maven demo project

---
+++ demo/src/main/java/com/pfe/App.java
@@ -0,0 +1,13 @@
+package com.pfe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
